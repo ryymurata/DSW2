f385d1f6c8c68102d9661e90ee0d4c3677c3526e
VLx2000 <victor.antunes.2000@gmail.com>
2022-03-31 19:44:35 -0300

    terminando cliente

--- frontend/js/cliente.js
+++ frontend/js/cliente.js
@@ -17,7 +17,7 @@ const linha = (cliente) => {
 				<td data-label="Sexo">${cliente.sexo}</td>
 				<td data-label="Nascimento">${cliente.nascimento}</td>
 				<td data-label="Ações">
-					<button onclick="editar(${cliente.id})"><span>&#x1F58C;</span></button>
+					<button onclick="gerar_edicao(${cliente.id})"><span>&#x1F58C;</span></button>
 					<button onclick="apagar(${cliente.id})"><span>&#x1F5D1;</span></button>
 				</td>
 			</tr>`
@@ -50,40 +50,80 @@ function apagar(id){
     xhr.send();
 }
 
-function criar(){
-	let novo_cliente = new Object();
-	novo_cliente.username  = document.getElementById("username").value;
-    novo_cliente.password = document.getElementById("password").value;
-    novo_cliente.role = document.getElementById("role").value;
-    novo_cliente.enabled = true;
-    novo_cliente.nome = document.getElementById("nome").value;
-    novo_cliente.telefone = document.getElementById("telefone").value;
-    novo_cliente.sexo = document.getElementById("sexo").value;
-    novo_cliente.nascimento = document.getElementById("nascimento").value;
-    novo_cliente.cpf = document.getElementById("CPF").value;
-
-	const json = JSON.stringify(novo_cliente);
-
-	let xhr = new XMLHttpRequest();
-    xhr.open('POST', API, true);
-    xhr.setRequestHeader("Content-type","application/json");
-    xhr.onload = function () {
-        if (this.status == 200) {
-            alert('Cliente criado!');
-        }
-		else{
-			alert('Ocorreu algum problema');
-		} 
-    }
-    xhr.send(json);
-	document.location.href = './clientes.html';
+function gerar_criacao(){
+    document.getElementById("username").value = '';
+    document.getElementById("password").value = '';
+    document.getElementById("role").value = '';
+    document.getElementById("nome").value = '';
+    document.getElementById("telefone").value = '';
+    document.getElementById("sexo").value = '';
+    document.getElementById("nascimento").value = '';
+    document.getElementById("CPF").value = '';
+    document.getElementById("indice").value = '';
+    document.getElementById("cadastro").style = 'display:block;';
+    document.getElementById("editando").innerHTML = 'Criando novo cliente';
 }
 
-function editar(id){
-	let xhr = new XMLHttpRequest();
-    xhr.open('PUT', API + '/' + id, true);
+function tabela(cliente){
+    document.getElementById("username").value = cliente.username;
+    document.getElementById("password").value = cliente.password;
+    document.getElementById("role").value = cliente.role;
+    document.getElementById("nome").value = cliente.nome;
+    document.getElementById("telefone").value = cliente.telefone;
+    document.getElementById("sexo").value = cliente.sexo;
+    document.getElementById("nascimento").value = cliente.nascimento;
+    document.getElementById("CPF").value = cliente.cpf;
+    document.getElementById("indice").value = cliente.id;
+    //console.log(cliente);
+    document.getElementById("cadastro").style = 'display:block;';
+    document.getElementById("editando").innerHTML = 'Editando ' + cliente.nome;
+}
+
+function gerar_edicao(id){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', API + '/' + id, true);
     xhr.onload = function() {
-        
+        if (this.readyState == 4 && this.status == 200) {
+            var cliente = JSON.parse(this.responseText);
+            //console.log(cliente);
+            tabela(cliente);
+        }
     }
     xhr.send();
+}
+
+function processar(){
+    let info_cliente = new Object();
+	info_cliente.username  = document.getElementById("username").value;
+    info_cliente.password = document.getElementById("password").value;
+    info_cliente.role = document.getElementById("role").value;
+    info_cliente.enabled = true;
+    info_cliente.nome = document.getElementById("nome").value;
+    info_cliente.telefone = document.getElementById("telefone").value;
+    info_cliente.sexo = document.getElementById("sexo").value;
+    info_cliente.nascimento = document.getElementById("nascimento").value;
+    info_cliente.cpf = document.getElementById("CPF").value;
+
+    const json = JSON.stringify(info_cliente);
+
+	let xhr = new XMLHttpRequest();
+    let id = document.getElementById("indice").value;
+    //console.log(id);
+    if (id != ''){
+        xhr.open('PUT', API + '/' + id, true);
+    }else{
+        xhr.open('POST', API, true);
+    }
+    
+    xhr.setRequestHeader("Content-type","application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.status === 200){
+            alert('Deu certo!');
+            document.location.reload(true);
+        }
+        else{
+            alert('Ocorreu algum problema');
+        } 
+    }
+    xhr.send(json);
 }
