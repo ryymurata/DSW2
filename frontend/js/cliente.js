3a800b0faf5081310326ec3e3dfe6c295662079c
VLx2000 <victor.antunes.2000@gmail.com>
2022-03-31 17:56:32 -0300

    adicionando remocao e adiçao de cliente

--- frontend/js/cliente.js
+++ frontend/js/cliente.js
@@ -12,21 +12,15 @@ const linha = (cliente) => {
 				<td data-label="Id" >${cliente.id}</td>
 				<td data-label="Nome" >${cliente.nome}</td>
 				<td data-label="Email">${cliente.username}</td>
-				<td data-label="CPF">${cliente.CPF}</td>
+				<td data-label="CPF">${cliente.cpf}</td>
 				<td data-label="Telefone">${cliente.telefone}</td>
 				<td data-label="Sexo">${cliente.sexo}</td>
 				<td data-label="Nascimento">${cliente.nascimento}</td>
-				<td data-label="Ações" colspan="2">
-					<a class="actions" th:href="@{/cliente/editar/{id} (id=${cliente.id})}" role="button">
-							<span th:title="#{object.update}" aria-hidden="true">&#x1F58C;</span>
-					</a>
-	
-					<a class="actions" th:href="@{/cliente/remover/{id} (id=${cliente.id})}" role="button">
-							<span th:title="#{object.delete}">&#x1F5D1;</span>
-					</a>
-
+				<td data-label="Ações">
+					<button onclick="editar(${cliente.id})"><span>&#x1F58C;</span></button>
+					<button onclick="apagar(${cliente.id})"><span>&#x1F5D1;</span></button>
 				</td>
-		</tr>`
+			</tr>`
 }
 
 function lista(){
@@ -37,6 +31,59 @@ function lista(){
             var clientes = JSON.parse(this.responseText);
             linhas(clientes);
         }
+    }
+    xhr.send();
+}
+
+function apagar(id){
+	let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', API + '/' + id, true);
+    xhr.onload = function() {
+        if (this.readyState == 4 && this.status == 204) {
+            alert('Cliente removido!');
+			document.location.reload(true);
+        }
+		else{
+			alert('Ocorreu algum problema');
+		}
+    }
+    xhr.send();
+}
+
+function criar(){
+	let novo_cliente = new Object();
+	novo_cliente.username  = document.getElementById("username").value;
+    novo_cliente.password = document.getElementById("password").value;
+    novo_cliente.role = document.getElementById("role").value;
+    novo_cliente.enabled = true;
+    novo_cliente.nome = document.getElementById("nome").value;
+    novo_cliente.telefone = document.getElementById("telefone").value;
+    novo_cliente.sexo = document.getElementById("sexo").value;
+    novo_cliente.nascimento = document.getElementById("nascimento").value;
+    novo_cliente.cpf = document.getElementById("CPF").value;
+
+	const json = JSON.stringify(novo_cliente);
+
+	let xhr = new XMLHttpRequest();
+    xhr.open('POST', API, true);
+    xhr.setRequestHeader("Content-type","application/json");
+    xhr.onload = function () {
+        if (this.status == 200) {
+            alert('Cliente criado!');
+        }
+		else{
+			alert('Ocorreu algum problema');
+		} 
+    }
+    xhr.send(json);
+	document.location.href = './clientes.html';
+}
+
+function editar(id){
+	let xhr = new XMLHttpRequest();
+    xhr.open('PUT', API + '/' + id, true);
+    xhr.onload = function() {
+        
     }
     xhr.send();
 }
